648623d6ee773a500ffaca0e79dee27447bbee93
vinibressanini <vinimarcus41@gmail.com>
2023-01-19 18:13:06 -0300

    Add project files.

---
+++ LogicRecap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LogicRecap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A soma dos dois números é: " + (number1 + number2));
+        }
+    }
+}
